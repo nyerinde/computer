1726d46996df6125c595ed98117e4e0175d87c64
nyerinde <nyerinde@iski.gov.tr>
2023-04-05 13:08:01 +0300

    Initial commit

---
+++ src/main/java/com/universal_yazilim/bid/ysm/computer_engineer/ComputerApplication.java
@@ -0,0 +1,13 @@
+package com.universal_yazilim.bid.ysm.computer_engineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universal_yazilim/bid/ysm/computer_engineer/ComputerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universal_yazilim.bid.ysm.computer_engineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
